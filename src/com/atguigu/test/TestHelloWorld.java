31ae23d5cb6515a1d890631950f5723b29ed3417
wuyj298 <wuyj298@163.COM>
2016-01-06 01:37:38 +0800

    update for egit 01
    update for egit 01

--- src/com/atguigu/test/TestHelloWorld.java
+++ src/com/atguigu/test/TestHelloWorld.java
@@ -4,5 +4,8 @@ public class TestHelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("hello world ....");
+		System.out.println("hello world ....");
+		System.out.println("hello world ....");
+		
 	}
 }
