1225fdd38d985c765aa3555256f4c298c79ac445
wuyj298 <wuyj298@163.COM>
2016-01-06 01:38:49 +0800

    update for egit 02
    update for egit 02

--- src/com/atguigu/test/TestHelloWorld.java
+++ src/com/atguigu/test/TestHelloWorld.java
@@ -6,6 +6,7 @@ public class TestHelloWorld {
 		System.out.println("hello world ....");
 		System.out.println("hello world ....");
 		System.out.println("hello world ....");
+		System.out.println("hello world ....");
 		
 	}
 }
